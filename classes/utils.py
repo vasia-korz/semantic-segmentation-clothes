052fc03b9157ff32ec3197ac906313a3c31bdf8a
Vasyl Korzavatykh <vasia@korzavatykh.com>
2025-01-13 18:22:43 +0100

    Update: Visualize predictions

--- classes/utils.py
+++ classes/utils.py
@@ -1,4 +1,5 @@
 import matplotlib.pyplot as plt
+import torch
 
 def visualize(pair):
     image, mask = pair
@@ -16,3 +17,38 @@ def visualize(pair):
     ax[1].axis('off')
     
     plt.show()
+
+
+def visualize_prediction(model, dataset_sample, device, class_labels=None):
+    image, mask = dataset_sample
+
+    image = image.unsqueeze(0).to(device)
+
+    model.eval()
+    with torch.no_grad():
+        output = model(image)
+    predicted_mask = torch.argmax(output, dim=1).squeeze(0).cpu().numpy()
+
+    plt.figure(figsize=(15, 5))
+
+    plt.subplot(1, 3, 1)
+    plt.imshow(image.squeeze(0).permute(1, 2, 0).cpu().numpy())
+    plt.title("Image")
+    plt.axis("off")
+
+    plt.subplot(1, 3, 2)
+    plt.imshow(mask.cpu().numpy())
+    plt.title("Ground Truth")
+    plt.axis("off")
+
+    plt.subplot(1, 3, 3)
+    plt.imshow(predicted_mask)
+    plt.title("Predicted")
+    plt.axis("off")
+
+    plt.show()
+
+    if class_labels:
+        print("\nClass Labels:")
+        for idx, label in class_labels.items():
+            print(f"{idx}: {label}")

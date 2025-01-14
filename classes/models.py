6d4ac6caa41f9d8f69f6c51776b81c0212d135e1
Andrew Chmutov <ulman.andre@gmail.com>
2025-01-14 01:47:50 +0100

    refactor: models
    Signed-off-by: Andrew Chmutov <ulman.andre@gmail.com>

--- classes/models.py
+++ classes/models.py
@@ -1,11 +1,21 @@
+from abc import ABC, abstractmethod
+
 import segmentation_models_pytorch as smp
+from torch._prims_common import DeviceLikeType
 
-from classes.clothes_model import ClothesModel
+from classes.clothes_model import ClothesModel, LossType, OptimType
 
 
-class UNetModel(ClothesModel):
-    def __init__(self, loss_fn, optimizer, lr, device, encoder="resnet34"):
-        model = smp.Unet(
+class SegmentationModel(ClothesModel, ABC):
+    def __init__(
+        self,
+        loss_fn: LossType,
+        optimizer: OptimType,
+        lr: float,
+        device: DeviceLikeType,
+        encoder: str = "resnet34",
+    ) -> None:
+        model = self.architecture()(
             encoder_name=encoder,  # ResNet34 as the backbone
             encoder_weights="imagenet",  # Pre-trained on ImageNet
             in_channels=3,  # RGB input
@@ -13,46 +23,37 @@ class UNetModel(ClothesModel):
         )
         super().__init__(model, loss_fn, optimizer, lr, device)
 
-
-class DeepLabModel(ClothesModel):
-    def __init__(self, loss_fn, optimizer, lr, device, encoder="resnet34"):
-        model = smp.DeepLabV3Plus(
-            encoder_name=encoder,  # ResNet50 backbone
-            encoder_weights="imagenet",  # Pre-trained weights
-            in_channels=3,  # RGB input
-            classes=59,  # Number of classes
-        )
-        super().__init__(model, loss_fn, optimizer, lr, device)
+    @abstractmethod
+    @staticmethod
+    def architecture() -> type:
+        ...
 
 
-class FPNModel(ClothesModel):
-    def __init__(self, loss_fn, optimizer, lr, device, encoder="resnet34"):
-        model = smp.FPN(
-            encoder_name=encoder,  # EfficientNet backbone
-            encoder_weights="imagenet",  # Pre-trained weights
-            in_channels=3,  # RGB input
-            classes=59,  # Number of classes
-        )
-        super().__init__(model, loss_fn, optimizer, lr, device)
+class UNetModel(SegmentationModel):
+    @staticmethod
+    def architecture() -> type:
+        return smp.Unet
 
 
-class LinkNetModel(ClothesModel):
-    def __init__(self, loss_fn, optimizer, lr, device, encoder="resnet34"):
-        model = smp.Linknet(
-            encoder_name=encoder,  # DenseNet backbone
-            encoder_weights="imagenet",  # Pre-trained weights
-            in_channels=3,  # RGB input
-            classes=59,  # Number of classes
-        )
-        super().__init__(model, loss_fn, optimizer, lr, device)
+class DeepLabModel(SegmentationModel):
+    @staticmethod
+    def architecture() -> type:
+        return smp.DeepLabV3Plus
 
 
-class PSPNetModel(ClothesModel):
-    def __init__(self, loss_fn, optimizer, lr, device, encoder="resnet34"):
-        model = smp.PSPNet(
-            encoder_name=encoder,  # ResNet50 backbone
-            encoder_weights="imagenet",  # Pre-trained weights
-            in_channels=3,  # RGB input
-            classes=59,  # Number of classes
-        )
-        super().__init__(model, loss_fn, optimizer, lr, device)
+class FPNModel(SegmentationModel):
+    @staticmethod
+    def architecture() -> type:
+        return smp.FPN
+
+
+class LinkNetModel(SegmentationModel):
+    @staticmethod
+    def architecture() -> type:
+        return smp.Linknet
+
+
+class PSPNetModel(SegmentationModel):
+    @staticmethod
+    def architecture() -> type:
+        return smp.PSPNet
